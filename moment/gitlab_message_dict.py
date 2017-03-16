fae03b67803bf4ebfd31e73cca6f4c7d1be9e9a5
ZhangBohan <bohanzhang@foxmail.com>
2017-03-16 11:22:52 +0800

    打开了一个新问题

--- moment/gitlab_message_dict.py
+++ moment/gitlab_message_dict.py
@@ -1,8 +1,9 @@
 
 
 def get_dingtalk_data(request_json):
+    print(f"type: {request_json['object_kind']}")
+
     if request_json['object_kind'] == 'push':
-        print(f"type: {request_json['object_kind']}")
         commit = request_json['commits'][0]
         data = {
             "msgtype": "link",
@@ -14,12 +15,21 @@ def get_dingtalk_data(request_json):
             }
         }
     elif request_json['object_kind'] == 'note':
-        print(f"type: {request_json['object_kind']}")
         data = {
             "msgtype": "link",
             "link": {
                 "title": f"{request_json['user']['name']} 评论了{request_json['project']['name']}",
                 "text": request_json['object_attributes']['note'],
+                "picUrl": request_json['user']['avatar_url'],
+                "messageUrl": request_json['object_attributes']['url']
+            }
+        }
+    elif request_json['object_kind'] == 'issue' and request_json['object_attributes']['action'] == 'open':
+        data = {
+            "msgtype": "link",
+            "link": {
+                "title": f"{request_json['user']['name']} 打开了一个新的问题",
+                "text": request_json['object_attributes']['title'],
                 "picUrl": request_json['user']['avatar_url'],
                 "messageUrl": request_json['object_attributes']['url']
             }
