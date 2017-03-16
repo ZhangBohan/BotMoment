b218bc6e2ff6e6e80a787a5cd41e2cb7fd1ade37
ZhangBohan <bohanzhang@foxmail.com>
2017-03-16 12:52:28 +0800

    优化推送数据格式

--- moment/gitlab_message_dict.py
+++ moment/gitlab_message_dict.py
@@ -43,26 +43,37 @@ def get_dingtalk_data(request_json):
 > ######  [@{project['name']}]({project['web_url']}) Assignee: **{assignee}** State: {issue['state']}"""
             }
         }
-    elif request_json['object_kind'] == 'issue' and request_json['object_attributes']['action'] == 'open':
-        data = {
-            "msgtype": "link",
-            "link": {
-                "title": f"{request_json['user']['name']} 打开了一个新的问题",
-                "text": request_json['object_attributes']['title'],
-                "picUrl": request_json['user']['avatar_url'],
-                "messageUrl": request_json['object_attributes']['url']
-            }
-        }
     elif request_json['object_kind'] == 'issue':
+        user = request_json['user']
+        project = request_json['project']
+        issue = request_json.get('issue', {})
+        assignee = request_json.get('assignee', {}).get('name', '')
+
+        if request_json['object_attributes']['action'] == 'open':
+            title = f"{request_json['user']['name']} 打开了一个新问题"
+            state = 'open'
+            title = request_json['object_attributes']['title']
+        else:
+            title = f"{request_json['user']['name']} 更新了问题"
+            state = issue['state']
+            title = issue['title']
+
+        text = f"""{user['name']}：**{title}**
+>  [{title}]({request_json['object_attributes']['url']})
+> {issue.get('description')}
+
+> ----
+
+> ######  [@{project['name']}]({project['web_url']}) Assignee: **{assignee}** State: {state}"""
+
         data = {
-            "msgtype": "link",
-            "link": {
-                "title": f"{request_json['user']['name']} 更新了问题",
-                "text": request_json['object_attributes']['title'],
-                "picUrl": request_json['user']['avatar_url'],
-                "messageUrl": request_json['object_attributes']['url']
+            "msgtype": "markdown",
+            "markdown": {
+                "title": "xxx",
+                "text": text
             }
         }
+
     else:
         print(json.dumps(request_json, ensure_ascii=False, indent=4))
 
