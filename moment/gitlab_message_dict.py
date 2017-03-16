dd48117c9b090a0487ee205f36ea5602e8ff223d
ZhangBohan <bohanzhang@foxmail.com>
2017-03-16 10:50:35 +0800

    增加gitlab note 支持

--- moment/gitlab_message_dict.py
+++ moment/gitlab_message_dict.py
@@ -2,6 +2,7 @@
 
 def get_dingtalk_data(request_json):
     if request_json['object_kind'] == 'push':
+        print(f"type: {request_json['object_kind']}")
         commit = request_json['commits'][0]
         data = {
             "msgtype": "link",
@@ -10,6 +11,17 @@ def get_dingtalk_data(request_json):
                 "text": commit['message'],
                 "picUrl": request_json['user_avatar'],
                 "messageUrl": commit['url']
+            }
+        }
+    elif request_json['object_kind'] == 'note':
+        print(f"type: {request_json['object_kind']}")
+        data = {
+            "msgtype": "link",
+            "link": {
+                "title": f"{request_json['user']['name']} 评论了{request_json['project']['name']}",
+                "text": request_json['object_attributes']['note'],
+                "picUrl": request_json['user']['avatar_url'],
+                "messageUrl": request_json['object_attributes']['url']
             }
         }
 
